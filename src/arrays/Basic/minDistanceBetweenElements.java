a6516a54eb3c4bda126ad9a5a52893d488180f7f
000deepak <kumbhardeepak000@gmail.com>
2022-08-17 19:33:29 +0530

    java concepts

--- src/arrays/Basic/minDistanceBetweenElements.java
+++ src/arrays/Basic/minDistanceBetweenElements.java
@@ -1,7 +1,30 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+package arrays.Basic;
 
-public class test {
+/**
+ * You are given an array A, of N elements. Find minimum index based distance between two elements of the array, x and y.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * N = 4
+ * A[] = {1,2,3,2}
+ * x = 1, y = 2
+ * Output: 1
+ * Explanation: x = 1 and y = 2. There are
+ * two distances between x and y, which are
+ * 1 and 3 out of which the least is 1.
+ * Example 2:
+ * <p>
+ * Input:
+ * N = 7
+ * A[] = {86,39,90,67,84,66,62}
+ * x = 42, y = 12
+ * Output: -1
+ * Explanation: x = 42 and y = 12. We return
+ * -1 as x and y don't exist in the array.
+ */
+
+public class minDistanceBetweenElements {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         System.out.println(minDist(arr, arr.length, 8, 5));
@@ -14,7 +37,6 @@ public class test {
         int[] yj = new int[n];
         // code here
         for (int i = 0; i < n; i++) {
-
             if (x == a[i]) {
                 xi[i] = i;
                 for (int j = 0; j < n; j++) {
@@ -24,7 +46,7 @@ public class test {
                         if (minDist == 0) minDist = yj[j] - xi[i];
                         checkDist = yj[j] - xi[i];
                         if (checkDist < minDist) return Math.abs(checkDist);
-                        else return minDist;
+                        else return Math.abs(minDist);
                     }
                 }
             }
