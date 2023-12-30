98d2ed42caa87b5c56469e045fbe68db67c09bb6
000deepak <kumbhardeepak000@gmail.com>
2023-12-30 14:11:31 +0530

    shiva-shambho

---
+++ src/Number/fibonaciSeries.java
@@ -0,0 +1,19 @@
+package Number;
+
+public class fibonaciSeries {
+    public static void main(String[] args) {
+        printFibonacciSeries(10);
+    }
+
+    private static void printFibonacciSeries(int n) {
+        int firstTerm = 0, secondTerm = 1;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(firstTerm + " ");
+
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+    }
+}
