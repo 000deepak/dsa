482d3c6849fcd99cb84197ba47e6ab3c801db50e
000deepak <kumbhardeepak000@gmail.com>
2024-05-03 23:26:06 +0530

    shiva-shambhao

--- src/string/Basic/Palindrome.java
+++ src/string/Basic/Palindrome.java
@@ -1,5 +1,8 @@
 package string.Basic;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Palindrome {
     public static void main(String[] args) {
         String s = "arora";
@@ -8,5 +11,68 @@ public class Palindrome {
         for (int i = s.length() - 1; i >= 0; i--) rev = rev + s.charAt(i);
         if (s.equals(rev)) System.out.println("palindrome");
         else System.out.println("not palindrome");
+    }
+
+    void checkIfPangram(sentence) {
+        let letters = new Set() ;
+        for(let i=0;i<sentence.length;i++){
+            let char = sentence[i].toLowerCase();
+            if(char >= 'a' && char <= 'z'){
+                letters.add(char);
+            }
+        }
+    return letters.size===26;
+    }
+
+    var mostWordsFound = function(sentences) {
+        let max = 0;
+        let temp = 0;
+        for (let i = 0; i < sentences.length; i++) {
+            temp = sentences[i].split(" ").length;
+            if (temp > max) {
+                max = temp;
+            }
+        }
+
+        return max;
+    };
+
+    var isPalindrome = function(s) {
+        s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+        let left=0;
+        let right = s.length-1;
+        while(left<right){
+            if(s[left]!==s[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    };
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            map.put(charS, map.getOrDefault(charS, 0) + 1);
+        }
+
+        for (int j = 0; j < t.length(); j++) {
+            char charT = t.charAt(j);
+
+            if (!map.containsKey(charT) || map.get(charT) == 0) {
+                return false;
+            }
+
+            map.put(charT, map.get(charT) - 1);
+        }
+
+        return true;
     }
 }
--- src/string/Basic/ReverseString.java
+++ src/string/Basic/ReverseString.java
@@ -4,7 +4,12 @@ import java.util.Arrays;
 
 public class ReverseString {
     public static void main(String[] args) {
+        String s = "arora";
+        String rev = "";
 
+        for (int i = s.length() - 1; i >= 0; i--) rev = rev + s.charAt(i);
+        if (s.equals(rev)) System.out.println("palindrome");
+        else System.out.println("not palindrome");
     }
 
     public static char[] sort(String s) {
