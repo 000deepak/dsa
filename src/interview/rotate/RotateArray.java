694ac5e6951410875a05bf23c821a7377c5c35d9
000deepak <kumbhardeepak000@gmail.com>
2024-05-12 14:28:37 +0530

    shiva-shambhao

--- src/interview/rotate/RotateArray.java
+++ src/interview/rotate/RotateArray.java
@@ -14,44 +14,18 @@ import java.util.Arrays;
  */
 public class RotateArray {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 7, 8};
+        int[] arr= {1, 2, 3, 4, 5};
+        int end = arr.length -1 ;
+        int d = 1;
 
-        // Method 1
-        int rotateSteps = 2;
-        int k = rotateSteps % arr.length -1;
-        System.out.println("k is " + k);
-        int start = 0;
-        int end = arr.length - 1;
-
-        for (int i = 0; i < k; i++) {
-            // Perform one step of rotation by reversing the array
-            reverse(arr, start, end);
-            end--; // Decrease the end index
+        for (int i = 1; i <= d; i++) {
+            int last = arr[0];
+            for (int j = 0; j < end; j++) {
+                arr[j] = arr[j + 1];
+            }
+            arr[end] = last;
         }
-        System.out.println("method 1 : " + Arrays.toString(arr));
-
-
-        //method 2
-        int[] ar = {1, 2, 3, 4, 5};
-        int n = ar.length;
-
-        int temp = ar[n - 1];
-        for (int i = 1; i < n; i++) {
-            int j = i + 1;
-            ar[n - i] = ar[n - j];
-        }
-        ar[0] = temp;
-        System.out.println("method 2 " + Arrays.toString(ar));
-    }
-
-    public static void reverse(int[] arr, int start, int end) {
-        while (start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
+        System.out.println("rotated array" + Arrays.toString(arr));//[2, 3, 4, 5, 1]
     }
 }
 /**
