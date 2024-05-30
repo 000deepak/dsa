4f709830d72d110713f7608e6f3cadd1bf51d1b0
000deepak <kumbhardeepak000@gmail.com>
2024-05-30 16:51:45 +0530

    shiva-shambho

--- src/intervieUnsolved.java
+++ src/intervieUnsolved.java
@@ -1,15 +1,22 @@
 public class intervieUnsolved {
 
-    //darby systems banglore
-    /*1*/
-    //String s = "abcdefg";
-    //int key = 2;
-    //output - aceg
-    /*sliding window question string*/
-    //implement lamda using functional interface
-    //difference between anonyous and lambda
-    //groupby query
+    /*
+    * infosys
+    * find duplicates in array
+    * reverse the sentence
+    *
+    * */
 
+    //darby systems banglore
+    /*
+    * String s = "abcdefg";
+    * int key = 2;
+    * output - aceg
+    * sliding window question string
+    * implement lamda using functional interface
+    * difference between anonyous and lambda
+    * groupby query
+    * */
 
     //iSynergy
     /*
@@ -30,7 +37,8 @@ public class intervieUnsolved {
      *
      */
 
-    /*Intellylabz
+    //Intellylabz
+    /*
     * hashmap java 8 new feaures
     * identity hashmap
     * concurrent hashmap
@@ -41,5 +49,55 @@ public class intervieUnsolved {
     * garbage collection working
     * types of garbage collectors
     * what is semaphores
+    * */
+
+
+    /*
+    * fulcrum digital
+    * metaspace in java 8
+    * crud vs jpa repo
+    * hashmap java 8 changes
+    * map and flatmap
+    * methods present in serialization
+    * why we need mockito if we have junit
+    * powermockito
+    * */
+
+    /*
+    * sankhayana consultancy
+    * reverse the stirg
+    * sorting alogrithm
+    * basic java and spring questions
+    *
+    * */
+
+    /*
+    * monocept
+    * slinding window problem - lngest substring with non repeating characters
+    *
+    * */
+
+
+    /*
+    * YuppTv
+    * subquery
+    * groupBy query with max emplyee salary for each dept (MAX fn)
+    * select dept.id,MAX(emp.salary) from employee emp join department dept on emp.departmentId = dept.id group by dept.id
+    *
+    * */
+
+    /*
+    * studio diseno
+    *
+    *
+    * */
+
+    /*
+    * fulcrum digital 2nd round
+    * code snipped of string and hashmap
+    * table views and partition
+    * code to take duplicate elements at end of array
+    * completable future
+    *
     * */
 }
--- src/java8/Test.java
+++ src/java8/Test.java
@@ -5,6 +5,8 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.stream.Nodes.collect;
+
 public class Test {
     public static void main(String[] args) {
         List<Employee> employees = new ArrayList<>();
@@ -14,46 +16,60 @@ public class Test {
         employees.add(new Employee("Mohan", 18, "RFP04", "Female", 50000));
         employees.add(new Employee("Rohan", 23, "RFP02", "Male", 10000));
 
-
         employees.stream().forEach(e -> System.out.println(e.getGender()));
 
-        var emp = employees.stream()
-                .distinct().sorted(Comparator.comparing(Employee::getName))
-                .skip(1).findFirst();
-//                .distinct()
-//                .sorted(Comparator.comparingDouble(Employee::getSalary))
-//                .skip(1)
-//                .findFirst();
+        var empName = employees.stream()
+                .distinct()
+                .sorted(Comparator.comparing(Employee::getName))
+                .skip(1)
+                .findFirst();
 
-        System.out.println(emp.get());
+        var empSalary = employees.stream()
+                .distinct()
+                .sorted(Comparator.comparingDouble(Employee::getSalary))
+                .skip(1)
+                .findFirst();
 
+
+        //reverse the string
         String str = "hello world hello";
         Integer i = 234;
         String s2 = String.valueOf(i);
         Stream.of(str).map(s -> new StringBuilder(s).reverse()).collect(Collectors.joining(""));
 
          Stream.of(s2)
-                .map(word -> new StringBuilder(word).reverse())
+                .map(word ->
+                        new StringBuilder(word)
+                                .reverse())
                  .forEach(System.out::println);
-//                .collect(Collectors.joining(" "));
+                //.collect(Collectors.joining(" "));
         System.out.println(s2);
 
-//         str.chars()
-//                 .mapToObj(c->(char)c)
-//                 .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
-//                 .entrySet()
-//                 .stream().filter(f->f.getValue()>1)
-//                 .forEach(s -> System.out.println(s.getKey()));
+        //duplicate characters
+         str.chars()
+                 .mapToObj(c->(char)c)
+                 .collect(Collectors
+                         .groupingBy(Function.identity(),Collectors.counting()))
+                 .entrySet()
+                 .stream().filter(f->f.getValue()>1)
+                 .forEach(s -> System.out.println(s.getKey()));
 
 
-//         List<String> list = Arrays.asList(str.split(" "));
+        //duplicate words
+        List<String> list = Arrays.asList(str.split(" "));
+        list.stream()
+                .collect(Collectors.groupingBy(String::toLowerCase, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() > 1)
+                .forEach((entry) -> System.out.println(entry.getKey()));
+
+//        //duplicate words
 //        list.stream()
-//                .collect(Collectors.groupingBy(String::toLowerCase, Collectors.counting()))
-//                        .entrySet().stream()
-//                .filter(entry -> entry.getValue() > 1)
-//                .forEach((entry) -> System.out.println(entry.getKey()));
-
-//        list.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
-//                .entrySet().stream().filter(f->f.getValue()>1).forEach(s-> System.out.println(s.getKey()));
+//                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
+//                .entrySet()
+//                .stream()
+//                .filter(f->f.getValue()>1)
+//                .forEach(s-> System.out.println(s.getKey()));
     }
 }
