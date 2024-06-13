6f4a7b8fac24f626e6f664318eb6040c989d3af2
000deepak <kumbhardeepak000@gmail.com>
2024-06-13 11:22:27 +0530

    shiva-shambho

---
+++ src/InterviewAttended/synasoft/Recursion.java
@@ -0,0 +1,19 @@
+package InterviewAttended.synasoft;
+
+import java.security.InvalidParameterException;
+
+public class Recursion {
+    public static void main(String[] args) {
+        System.out.println(factorial(-5));
+
+    }
+
+    static int factorial(int i){
+        if(i == 0 || i == 1) return 1;
+        else if (i < 0) {
+            throw new InvalidParameterException("invalid number");
+        }
+
+        return i * factorial(i-1);
+    }
+}
