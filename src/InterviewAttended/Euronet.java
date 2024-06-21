a9d4c6e1848673d404eb44d56b5d49f4b67e38f7
000deepak <kumbhardeepak000@gmail.com>
2024-06-21 16:36:43 +0530

    shiva-shambho

---
+++ src/InterviewAttended/Euronet.java
@@ -0,0 +1,106 @@
+package InterviewAttended;
+
+public class Euronet {
+
+
+    String s = "Deepakashokkumar"
+
+    Map<Character,Integer> charFreq = new HashMap<>();
+
+for(char c: s.toCharArray()){
+        charFreq.put(c, charFreq.getOrDefault(c, 0)+1);
+
+    }
+
+charFreq.forEach((k,v)->sout("key is: " + k + " " + "frequency is " + v)
+
+    List<Employee> empList = 1000 emp details
+
+    empList.stream.
+.distinct()
+.sorted(Comparing.comparator(Employee::getSalary))
+            .skip(2)
+.findFirst();
+
+
+
+    Class CustomException extends Exception{
+
+public CustomException(){
+            super("this is custom exception")
+
+        }
+    }
+
+    class Test{
+
+
+        public int test(){
+            try{
+                return 10;
+
+            }catch(){
+                return 20;
+
+            }finally{
+                return 30;
+
+            }
+
+
+        }
+
+
+
+        t1, t2
+
+        method even
+        method odd
+
+        t1 -> method even -> t1 wait t2 -> print number -> notify t2
+
+        class Test{
+
+            private final Object lock = new Object();
+            Boolean evenTurn = false;
+
+            publid static void main(){
+
+                Runnable r = Runnable{
+                    void run(){
+                    }
+                }
+
+                Thread t1 = new Thread(r);
+                Thread t2 = new Thread(r)
+
+            }
+
+            public void even(){
+                synchronized(lock){
+                    sout(even);
+
+                }
+
+                evenTurnj = false;
+                lock.notify();
+            }
+
+
+            public void odd(){
+
+                while(evenTurn){
+                    sout(odd);
+                    lock.wait()
+                }
+
+
+
+            }
+        }
+
+
+
+
+
+    }
