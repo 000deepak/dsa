366f11a6acb90232036ddf2666d9ab32028d47ef
000deepak <90568067+000deepak@users.noreply.github.com>
2024-12-19 16:41:53 +0530

    Update Fibonacci.java

--- src/Number/Fibonacci.java
+++ src/Number/Fibonacci.java
@@ -9,7 +9,8 @@ public class Fibonacci {
     static void Fib(int n) {
         int n1 = 0, n2 = 1, n3;
 
-        for (int i = 2; i < n; ++i) {
+        //start from 2
+        for (int i = 2; i < n; i++) {
             n3 = n1 + n2;
             System.out.print(" " + n3);
             n1 = n2;
